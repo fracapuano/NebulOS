5630c7a06db192d3a31b0d66e358235e6f49a6af
fracapuano <francesco_capuano@aol.com>
2023-08-05 16:49:18 +0200

    add: available datasets and devices

--- commons/utils.py
+++ commons/utils.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
+DATASETS = ["cifar10", "cifar100", "ImageNet16-120"]
+DEVICES = ["edgegpu", "eyeriss", "fpga"]
+
 def get_project_root(): 
     """
     Returns project root directory from this script nested in the commons folder.
